095678fa3431e56da6799d5c6824b948bcfc741a
Mahmoud Samy <mahmoudsamy@192.168.1.16>
2024-09-27 20:55:29 +0300

    update

--- _atoi.c
+++ _atoi.c
@@ -1,74 +1,48 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
- */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
-
-/**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
- */
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
-
-/**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ *_atoi - string to int
+ *@s: this is the source to copy
+ *Return: copy of original source
  */
 
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+	int res = 0;
+	int i;
+	int sign = 1;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0'; ++i)
 	{
+		if (s[i] == ' ')
+			continue;
 		if (s[i] == '-')
+		{
 			sign *= -1;
+			continue;
+		}
+
+		if (s[i] == '+')
+		{
+			continue;
+		}
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			res = res * 10 + s[i] - '0';
 		}
-		else if (flag == 1)
-			flag = 2;
+		else if (res == 0)
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
 	}
 
-	if (sign == -1)
-		output = -result;
+	if (res == 0)
+		return (0);
 	else
-		output = result;
-
-	return (output);
+		return (sign * res);
 }
